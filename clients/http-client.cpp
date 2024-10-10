7420b30d0e08ef9ecf659c77c6e3714568ae9542
Mohamad Ahmadi <mohamad.j.ahmadi@gmail.com>
2024-10-10 11:35:43 +0330

    Client: Add input file error handling

--- clients/http-client.cpp
+++ clients/http-client.cpp
@@ -38,142 +38,163 @@ using base64 = cppcodec::base64_rfc4648;
 // Performs an HTTP GET and prints the response
 int main(int argc, char **argv)
 {
+  try
+  {
+    // Check command line arguments.
+    if (argc != 4 && argc != 5)
+    {
+      std::cerr << "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n"
+                << "Example:\n"
+                << "    http-client-sync www.example.com 80 /\n"
+                << "    http-client-sync www.example.com 80 / 1.0\n";
+      return EXIT_FAILURE;
+    }
+    auto const host = argv[1];
+    auto const port = argv[2];
+    auto const image_path = argv[3];
+    int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
+
+
+     // Open the image file in binary mode
+    std::ifstream file(image_path, std::ios::binary);
+
+    // Check if the file was opened successfully
+    if (!file) {
+        std::cerr << "Error: Could not open file: " << image_path << std::endl;
+        return 1;
+    }
+
+    // Check if the file is empty by moving the file pointer to the end and checking the position
+    file.seekg(0, std::ios::end);
+    std::streampos file_size = file.tellg();
+    if (file_size == 0) {
+        std::cerr << "Error: File is empty: " << image_path << std::endl;
+        return 1;
+    }
+
+    // Move the file pointer back to the beginning to read the data
+    file.seekg(0, std::ios::beg);
+
+    // The io_context is required for all I/O
+    net::io_context ioc;
+
+    // These objects perform our I/O
+    tcp::resolver resolver(ioc);
+    beast::tcp_stream stream(ioc);
+    tcp::socket socket(ioc);
+    // Look up the domain name
+    auto const results = resolver.resolve(host, port);
+
+    // Make the connection on the IP address we get from a lookup
+
     try
     {
-        // Check command line arguments.
-        if (argc != 4 && argc != 5)
-        {
-            std::cerr << "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n"
-                      << "Example:\n"
-                      << "    http-client-sync www.example.com 80 /\n"
-                      << "    http-client-sync www.example.com 80 / 1.0\n";
-            return EXIT_FAILURE;
-        }
-        auto const host = argv[1];
-        auto const port = argv[2];
-        auto const target = argv[3];
-        int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
-
-        // The io_context is required for all I/O
-        net::io_context ioc;
-
-        // These objects perform our I/O
-        tcp::resolver resolver(ioc);
-        beast::tcp_stream stream(ioc);
-         tcp::socket socket(ioc);
-        // Look up the domain name
-        auto const results = resolver.resolve(host, port);
-
-        // Make the connection on the IP address we get from a lookup
-
-        try
-        {
-            auto x = net::connect(socket, results.begin(), results.end());
-        }
-        catch (std::exception &e)
-        {
-            std::cout << e.what() << std::endl;
-            return 1;
-        }
+      auto x = net::connect(socket, results.begin(), results.end());
+    }
+    catch (std::exception &e)
+    {
+      std::cout << e.what() << std::endl;
+      return 1;
+    }
 
 #ifdef POST
-        // Set up an HTTP POST request message
-        // send img
-        std::ifstream file(target, std::ios::binary);
-        std::vector<unsigned char> image_data((std::istreambuf_iterator<char>(file)),
-                                               std::istreambuf_iterator<char>());
-        std::string encoded_image = base64::encode(image_data);
+    // Set up an HTTP POST request message
+    // send img
+    //std::ifstream file(image_path, std::ios::binary);
+    std::vector<unsigned char> image_data((std::istreambuf_iterator<char>(file)),
+                                          std::istreambuf_iterator<char>());
+    std::string encoded_image = base64::encode(image_data);
 
-        // Create the JSON body
-        json json_body;
-        json_body["img"] = encoded_image;
-        //json_body["ConvertColorToGray"] = false;  // Set flag to convert to grayscale
-        //json_body["DetectEdges"] = true;
-        //json_body["ResizeImage"] = true;
-        //json_body["ResizeImage"]["width"] = 100;
-        //json_body["ResizeImage"]["height"] = 100;
-        //json_body["ApplyBlur"] = true;
-        //json_body["BlurKernelSize"] = 15;
-        //json_body["RotateImage"]["angle"] = 45;
-        //json_body["AdjustBrightnessContrast"]["brightness"] = 95;
-        //json_body["AdjustBrightnessContrast"]["contrast"] = 100;
-        json_body["ApplySharpening"] = true;
-        // Serialize JSON to string
-        std::string body = json_body.dump();
+    // Create the JSON body
+    json json_body;
+    json_body["img"] = encoded_image;
+    // json_body["ConvertColorToGray"] = false;  // Set flag to convert to grayscale
+    // json_body["DetectEdges"] = true;
+    // json_body["ResizeImage"] = true;
+    // json_body["ResizeImage"]["width"] = 100;
+    // json_body["ResizeImage"]["height"] = 100;
+    // json_body["ApplyBlur"] = true;
+    // json_body["BlurKernelSize"] = 15;
+    // json_body["RotateImage"]["angle"] = 45;
+    // json_body["AdjustBrightnessContrast"]["brightness"] = 95;
+    // json_body["AdjustBrightnessContrast"]["contrast"] = 100;
+    json_body["ApplySharpening"] = true;
+    // Serialize JSON to string
+    std::string body = json_body.dump();
 
-        // Create HTTP request
-        http::request<http::string_body> req{http::verb::post, "/", 11};
-        req.set(http::field::host, host);
-        req.set(http::field::content_type, "application/json");
-        req.body() = body;
-        req.content_length(body.size());
+    // Create HTTP request
+    http::request<http::string_body> req{http::verb::post, "/", 11};
+    req.set(http::field::host, host);
+    req.set(http::field::content_type, "application/json");
+    req.body() = body;
+    req.content_length(body.size());
 
-        // Send the request
-        http::write(socket, req);
-        
-        // Receive the HTTP response
+    // Send the request
+    http::write(socket, req);
 
-             // Receive the response
-        beast::flat_buffer buffer;
-        http::response<http::dynamic_body> res;
-        http::read(socket, buffer, res);
+    // Receive the HTTP response
 
-        std::cout << res << std::endl;
+    // Receive the response
+    beast::flat_buffer buffer;
+    http::response<http::dynamic_body> res;
+    http::read(socket, buffer, res);
 
-        // Save the response image (assumed base64 encoded)
-        json response_json = json::parse(beast::buffers_to_string(res.body().data()));
-        std::string response_image_base64 = response_json["processed_image"];
-        std::vector<unsigned char> decoded_image = base64::decode(response_image_base64);
+    std::cout << res << std::endl;
 
-        std::ofstream output_file("received_gray_image.jpg", std::ios::binary);
-        output_file.write(reinterpret_cast<const char*>(decoded_image.data()), decoded_image.size());
-        output_file.close();
+    // Save the response image (assumed base64 encoded)
+    json response_json = json::parse(beast::buffers_to_string(res.body().data()));
+    std::string response_image_base64 = response_json["processed_image"];
+    std::vector<unsigned char> decoded_image = base64::decode(response_image_base64);
+
+    std::ofstream output_file("received_gray_image.jpg", std::ios::binary);
+    output_file.write(reinterpret_cast<const char *>(decoded_image.data()), decoded_image.size());
+    output_file.close();
 
 #endif
 #ifdef GET
-        // Set up an HTTP GET request message
-        http::request<http::string_body> req{http::verb::get, target, version};
-        req.set(http::field::host, host);
-        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    // Set up an HTTP GET request message
+    http::request<http::string_body> req{http::verb::get, target, version};
+    req.set(http::field::host, host);
+    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-        // Send the HTTP request to the remote host
-        http::write(stream, req);
+    // Send the HTTP request to the remote host
+    http::write(stream, req);
 
-        // This buffer is used for reading and must be persisted
-        beast::flat_buffer buffer;
+    // This buffer is used for reading and must be persisted
+    beast::flat_buffer buffer;
 
-        // Declare a container to hold the response
-        http::response<http::dynamic_body> res;
+    // Declare a container to hold the response
+    http::response<http::dynamic_body> res;
 
-        // Receive the HTTP response
-        http::read(stream, buffer, res);
+    // Receive the HTTP response
+    http::read(stream, buffer, res);
 
-        // Write the message to standard out
-        std::cout << res.body().size() << std::endl;
-        // res.body.data();
-        FILE *pFile;
-        pFile = fopen("./myfile.jpeg", "w");
-        fwrite(boost::beast::buffers_to_string(res.body().data()).data(), 1, res.body().size(), pFile);
-        fclose(pFile);
+    // Write the message to standard out
+    std::cout << res.body().size() << std::endl;
+    // res.body.data();
+    FILE *pFile;
+    pFile = fopen("./myfile.jpeg", "w");
+    fwrite(boost::beast::buffers_to_string(res.body().data()).data(), 1, res.body().size(), pFile);
+    fclose(pFile);
 #endif
-        // Gracefully close the socket
-        beast::error_code ec;
-        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+    // Gracefully close the socket
+    beast::error_code ec;
+    stream.socket().shutdown(tcp::socket::shutdown_both, ec);
 
-        // not_connected happens sometimes
-        // so don't bother reporting it.
-        //
-        if (ec && ec != beast::errc::not_connected)
-            throw beast::system_error{ec};
+    // not_connected happens sometimes
+    // so don't bother reporting it.
+    //
+    if (ec && ec != beast::errc::not_connected)
+      throw beast::system_error{ec};
 
-        // If we get here then the connection is closed gracefully
-    }
-    catch (std::exception const &e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+    // If we get here then the connection is closed gracefully
+  }
+  catch (std::exception const &e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
 
 //]
